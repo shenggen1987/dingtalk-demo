52ad7a003cdfc7ffaa31fad8f15b79571f2cd891
xiexuemei <xiexuemei@xiexuemeideMacBook-Air.local>
2016-08-24 10:19:59 +0800

    add sass

--- src/button.jsx
+++ src/button.jsx
@@ -2,7 +2,7 @@ var React    = require('react'),
     ReactDOM = require('react-dom');
 
 var Tooltip = require('uxcore-tooltip');
-
+import './style/button.scss'
 ReactDOM.render(
   <Tooltip overlay="提示文字">
     <span>鼠标移上来就会出现提示</span>
--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
       {
         test: /\.less$/,
         loader: "style!css!less"
+      },
+      {
+        test: /\.scss$/,
+        loader: "style!css!sass"
       }
     ]
   },
