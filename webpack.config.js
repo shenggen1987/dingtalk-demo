ddb630e68ab5140dc2a2045b17cd9ac90518232f
xiexuemei <xiexuemei@xiexuemeideMacBook-Air.local>
2016-08-25 15:58:51 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var glob = require('glob');
 
 var definePlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
@@ -7,18 +8,30 @@ var definePlugin = new webpack.DefinePlugin({
 
 var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 
+var getEntry = function () {
+    var entry = {};
+    glob.sync('./src/**/*.jsx').forEach(function (name) {
+        if(name.indexOf('/components/') < 0){
+          var n = name.slice(6, name.length - 4);
+          entry[n] = name;
+        }
+    });
+    // console.log(entry);
+    // {
+    //   main:  './src/index.jsx',
+    //   other: './src/other.jsx',
+    //   button: './src/button.jsx',
+    //   dialog: './src/dialog.jsx',
+    //   select: './src/select.jsx',
+    //   datepicker: './src/datepicker.jsx',
+    //   table: './src/table.jsx',
+    //   tablemock: './src/tablemock.jsx'
+    // }
+    return entry;
+};
 module.exports = {
   cache: true,
-  entry: {
-    main:  './src/index.jsx',
-    other: './src/other.jsx',
-    button: './src/button.jsx',
-    dialog: './src/dialog.jsx',
-    select: './src/select.jsx',
-    datepicker: './src/datepicker.jsx',
-    table: './src/table.jsx',
-    tablemock: './src/tablemock.jsx'
-  },
+  entry: getEntry(),
   output: {
     path: 'public/build',
     filename: '[name].js'
