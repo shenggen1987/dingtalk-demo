c63c91d7556aeb35f7bcb613f1928f34dece8935
xiexuemei <xiexuemei@xiexuemeideMacBook-Air.local>
2016-08-26 09:25:55 +0800

    add mongo

--- backend/controller/movies.js
+++ backend/controller/movies.js
@@ -2,11 +2,18 @@ var Models = require('../models');
 exports.List = function(req, res, next) {
 	Models.Movies.find({
   }).exec(function (err, data) {
-  		console.dir(err, data);
   		return res.send(data);
   });
 	// return res.send({
 	// 	id: 1,
 	// 	name: '111'
 	// });
+}
+
+exports.getById = function(req, res, next){
+	Models.Movies.findOne({
+		_id: req.query.id
+  }).exec(function (err, data) {
+  		return res.send(data);
+  });
 }
--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -3,4 +3,6 @@ var router = express.Router();
 
 var Movies = require('../controller/movies');
 router.get('/list', Movies.List);
+router.get('/getById', Movies.getById);
+
 module.exports = router;
