31244e1ea8fefb9111da39fa96ec896ab17f6fcc
lishenggen <lishenggen@qjdchina.com>
2016-09-17 13:02:59 +0800

    dingtalk and weui

--- src/components/dialog/confirm.js
+++ src/components/dialog/confirm.js
@@ -4,12 +4,50 @@
 
 
 class Confirm extends React.Component {
-    
+    static propTypes = {
+        buttons: React.PropTypes.array,
+        show: React.PropTypes.bool,
+        title: React.PropTypes.string
+    };
+
+    static defaultProps = {
+        buttons: [],
+        show: false,
+        title: ''
+    };
 
     render() {
         const {title, show, children} = this.props;
+        const buttonName = this.props.buttons.map((action, idx) => {
+        	const {type, label, ...others} = action;
+        	return label;
+        });
         return (
-            <div>title</div>
+            <div>{
+            function(obj){
+              if(obj.props.show)
+                  return (function() {
+										    dd.device.notification.confirm({
+												    message: children,
+										        title: title,
+										        buttonName: buttonName,
+												    onSuccess : function(result) {
+												        //onSuccess将在点击button之后回调
+												        /*
+												        {
+												            buttonIndex: 0 //被点击按钮的索引值，Number类型，从0开始
+												        }
+												        */
+												    },
+												    onFail : function(err) {}
+												});
+
+
+										}())
+              else
+                  return ""
+              }(this)
+            }</div>	
         );
     }
 }
--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,23 @@
 import routes from './routes';
 
+dd.config({
+    agentId: '',
+    corpId: _config.corpId,
+    timeStamp: _config.timeStamp,
+    nonceStr: _config.nonceStr,
+    signature: _config.signature,
+    jsApiList: ['device.notification.confirm',
+        'device.notification.alert',
+        'device.notification.prompt',
+        'biz.chat.chooseConversation',
+        'biz.ding.post']
+});
+
+
+dd.error(function(err) {
+    alert('dd error: ' + JSON.stringify(err));
+});
+
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(routes, document.getElementById('App'));
 });
